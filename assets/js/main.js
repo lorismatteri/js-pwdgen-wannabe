e083bb0b5ef5d06b71508dfcecfa03e96fb58a14
Loris Matteri <loris.matteri93@gmail.com>
2020-11-12 16:03:41 +0100

    Primo commit nome e cognome

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+// Nome
+var name;
+
+var name = prompt("Qual è il tuo nome?");
+
+//Cognome
+var surname;
+
+var surname = prompt("Qual è il tuo cognome?")
+
+document.getElementById("title").innerHTML = name + " " + surname;
