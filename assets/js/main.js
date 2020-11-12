68054101dcfe6ca63ee81fe5b71c518145e3d160
Loris Matteri <loris.matteri93@gmail.com>
2020-11-12 16:15:09 +0100

    Colore + Password(idiota)

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,17 @@ var name = prompt("Qual è il tuo nome?");
 //Cognome
 var surname;
 
-var surname = prompt("Qual è il tuo cognome?")
+var surname = prompt("Qual è il tuo cognome?");
 
 document.getElementById("title").innerHTML = name + " " + surname;
+
+// Colore
+var favcolor;
+
+var favcolor = prompt("Qual'è il tuo colore preferito?");
+
+document.getElementById("color").innerHTML = favcolor;
+
+// Password poco sicura
+document.getElementById("parag").innerHTML = "La tua password idiota:"
+document.getElementById("password").innerHTML = name + surname + favcolor + 19
